b42ac0b652a6a2d48157a0d8167d03be479c1edd
Casey <halipheomos@hotmail.com>
2023-05-11 16:43:21 -0400

    Second.. initial commit?

--- Scenes/Platform Sections/world.gd
+++ Scenes/Platform Sections/world.gd
@@ -2,13 +2,9 @@ extends Node2D
 
 @onready var platform = preload("res://Scenes/Platform Sections/platform.tscn")
 
-#build an array of 10 or so platforms and each time a platform is removed, bump it off the array. Then add a new platform at coordinates far away.
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
