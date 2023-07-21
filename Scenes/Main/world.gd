7f47a7e74f955c49eee4b4e9ec95bb516b5e605a
Casey <halipheomos@hotmail.com>
2023-07-20 22:40:08 -0400

    Working Spawn Platform Code. Correct x-coordinate incrementation.

--- Scenes/Main/world.gd
+++ Scenes/Main/world.gd
@@ -6,7 +6,7 @@ extends Node2D
 
 @export var platform_limit = 20 # configurable number of platforms allowed to exist at one time before despawn
 
-var x_coordinate = 304 # this is the x coordinate of the last pixel of the start platform
+var x_coordinate = 0 # this is the x coordinate of the last pixel of the start platform
 var platform_count = 0 # number of platforms currently spawned
 
 @onready var platforms = [
@@ -24,18 +24,19 @@ func _physics_process(_delta):
 	else:
 		pass
 
-func increment_spawn_x_value():
+func increment_spawn_x_value(platform_length):
 	x_coordinate = randomize_blank_space(x_coordinate)
+	x_coordinate = x_coordinate + platform_length
 	return x_coordinate
 
 func spawn_platform():
 	var randomPlatformSelect = randi_range(0,2)
 	var newPlatform = platforms[randomPlatformSelect].instantiate()
-	var spawn_x = increment_spawn_x_value()
 	var spawn_y = randomize_y()
 	get_tree().current_scene.add_child(newPlatform) 
 	var platform_length_increment = newPlatform.pixel_length
-	newPlatform.position = Vector2(spawn_x+platform_length_increment,spawn_y)
+	var spawn_x = increment_spawn_x_value(platform_length_increment)
+	newPlatform.position = Vector2(spawn_x,spawn_y)
 	platform_count = platform_count+1
 
 func can_new_platform_spawn(count, limit):
@@ -49,7 +50,7 @@ func randomize_y():
 	return random_y
 
 func randomize_blank_space(x):
-	var random_addition = randi_range(300,500)
+	var random_addition = randi_range(100,150)
 	var new_x = random_addition + x
 	return new_x
 
