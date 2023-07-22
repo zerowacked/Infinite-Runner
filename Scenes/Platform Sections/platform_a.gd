b987c99166ba568d83697ed389a854d434ef7eb4
Casey <halipheomos@hotmail.com>
2023-07-22 01:20:20 -0400

    Deleting more platforms.

--- Scenes/Platform Sections/platform_a.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var pixel_length = 576:
-	get:
-		return pixel_length
-	set(value):
-		pixel_length = pixel_length + value
--- Scenes/Platform Sections/platform_b.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var pixel_length = 448:
-	get:
-		return pixel_length
-	set(value):
-		pixel_length = pixel_length + value
